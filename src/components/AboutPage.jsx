6956bae20d2e5a34ab201c002e646f26ed391d41
Gabo <gaabf@hotmail.com>
2023-02-18 16:58:19 -0500

    UPDATE: About Page image added and styling updated

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,16 +2,16 @@ function AboutPage(){
 
   return(
   <div>
-    <div className='h-50 bg-yellow-300 px-10 py-4'>
+    <div className='flex space-x-40 justify-center h-50 bg-yellow-300 px-10 py-4'>
       <div className='flex flex-col justify-center'>
         <div className='font-bold text-3xl'>
           Hola!
         </div>
-        <div>
+        <div className="max-w-md">
         Pariatur quas consequatur aut. Dolorum est quis eum. Placeat dolorem provident animi aliquam molestias sunt asp.
         </div>
       </div>
-      <href></href>
+      <img className="max-h-36 border-2 border-black" src='src/assets/images/madvillainy.png'></img>
     </div>
   </div>
   )

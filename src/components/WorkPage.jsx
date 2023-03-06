2c6a1d0a60f6b548d43079d79554a3b2fbb0973e
Gabo <gaabf@hotmail.com>
2023-03-06 12:26:59 -0500

    ADD: WorkPage basic content added. Styling pending

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -1,14 +1,89 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
 
 function WorkPage(){
 
   return(
     <div>
       <Header/>
+      <MainContainer>
+        <div>
+          <div className="text-3xl font-bold">
+            Work
+          </div>
+          <div>
+          Est quas et voluptatem vero distinctio. Molestiae eum aliqua..
+          </div>
+        </div>
 
+        <div>
+          <div className="text-2xl font-bold text-left">
+             Selected Works
+          </div>
+          <div className="flex flex-col">
+            <img src='src/assets/images/Rectangle 2banana.png'/>
+            <div className="font-bold text-left">Banana Library</div>
+            <div className="text-left">
+            Possimus sunt autem doloremque enim quo sit architecto reiciendis. Ipsum quam aut aut quo. Est quas et voluptatem vero distinctio. Molestiae eum aliqua...
+            </div>
+          </div>
+          <div className="flex">
+            <div className='flex items-center justify-center'>
+              <div className="flex flex-col items-center py-3">
+                <img className='min-w-full' src='src/assets/images/Rectangle 2image1.png'/>
+                <div className="font-bold self-start py-3">
+                  Float Life
+                </div>
+                <div className="text-left ">
+                  Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos.
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <img className='min-w-full' src='src/assets/images/Rectangle 2image2.png'/>
+                <div className="font-bold self-start py-3 text-left">
+                  Retro Call
+                </div>
+                <div className="text-left">
+                  Distinctio aperiam maiores et mollitia necessitatibus. Et reprehenderit.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="font-bold text-2xl text-left">Side Project</div>
 
+            <div className="flex items-center justify-center py-5">
+              <img src='src/assets/images/Rectangle 2small.png'/>
+              <div className="flex flex-col px-5">
+                <div className=" font-bold text-left">Buckinghamshire Croatian Quantify</div>
+                <div className="text-left">Expedita et eveniet non accusantium perspiciatis. Quam deleniti pariatur optio. Quo dicta voluptate...</div>
+                <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
+              </div>
+            </div>
+            <div>
+              <div className="font-bold text-left">Club transform</div>
+              <div className="text-left">
+              Voluptatum vel repudiandae occaecati. Mollitia quae perferendis dolore accusantium vel id. Voluptas perferendis id quasi amet in a qu...
+              </div>
+              <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="font-bold text-2xl text-left">Experiments & Works in progress</div>
+          <div>
+            <div className="font-bold text-left">
+              Rubber
+            </div>
+            <div className="text-left">
+              Aut amet nihil mollitia dolorem commodi quidem unde. Voluptas quaerat numquam quis sunt quas ab asperiores. Iste totam enim voluptas quo...
+            </div>
+            <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
+          </div>
+        </div>
 
+      </MainContainer>
       <Footer/>
     </div>
   )

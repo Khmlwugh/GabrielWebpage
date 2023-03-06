52c6a7641e68b1b27aca754c582a54cd46db9565
Gabo <gaabf@hotmail.com>
2023-03-06 10:11:57 -0500

    UPDATE: Home Page updated using MainContainer comp

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,6 +1,7 @@
 import Button from "./Buttons"
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
+import MainContainer from "./MainContainer"
 
 function IntroPage(){
    
@@ -23,25 +24,26 @@ function IntroPage(){
           Icons
         </div>
       </div>
-
+      <MainContainer>
+        
       <div className="flex flex-col items-center">
-        <div className="font-bold text-2xl py-10 w-6/12">Case Studies</div>
+        <div className="font-bold text-2xl py-10">Case Studies</div>
         <div className='flex items-center justify-center'>
-          <div className="flex flex-col items-center px-30 py-3">
-            <img className='h-6/12' src='src/assets/images/Rectangle 2image1.png'/>
-            <div className="font-bold self-start py-3 w-100 ">
+          <div className="flex flex-col items-center py-3">
+            <img className='min-w-full' src='src/assets/images/Rectangle 2image1.png'/>
+            <div className="font-bold self-start py-3">
               Float Life
             </div>
-            <div className="w-7/12 text-left ">
+            <div className="text-left ">
               Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos.
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <img className='h-6/12' src='src/assets/images/Rectangle 2image2.png'/>
-            <div className="font-bold self-start px-24 py-3 text-left w-100">
+            <img className='min-w-full' src='src/assets/images/Rectangle 2image2.png'/>
+            <div className="font-bold self-start py-3 text-left">
               Retro Call
             </div>
-            <div className="w-7/12 text-left">
+            <div className="text-left">
               Distinctio aperiam maiores et mollitia necessitatibus. Et reprehenderit.
             </div>
           </div>
@@ -49,7 +51,7 @@ function IntroPage(){
       </div>
 
       <div className="py-10">
-        <div className="font-bold text-2xl ">Side Project</div>
+        <div className="font-bold text-2xl text-left">Side Project</div>
 
         <div className="flex items-center justify-center py-5">
           <img src='src/assets/images/Rectangle 2small.png'/>
@@ -59,23 +61,24 @@ function IntroPage(){
             <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
           </div>
         </div>
-        <div className="flex flex-col items-center py-5">
-          <div className='font-bold text-left text-2xl py-5 w-100'>Recent Writing</div>
-          <div className="flex flex-col w-6/12">
+        <div className="flex flex-col py-5">
+          <div className='font-bold text-left text-2xl py-5'>Recent Writing</div>
+          <div className="flex flex-col">
             <div className="text-2xl font-bold text-left">Vortals convergence</div>
             <div className='text-left'>Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq...</div>
             <div className="text-left text-sm text-gray-500">Thu Jan 09, 2031 </div>
           </div>
 
-          <div className="flex flex-col w-6/12 py-5">
+          <div className="flex flex-col py-5">
             <div className="text-2xl font-bold text-left">Unbranded navigate</div>
             <div className='text-left'>Veniam consequatur distinctio. Sit dicta non odit ut. Eum iure dolor in et odit. Consectetur quis ut sit ducimus.</div>
             <div className="text-left text-sm text-gray-500">Fri Jun 11, 2027</div>
           </div>
         </div>
+        <Button text='View all Posts'/>
       </div>
-      <Button text='View all Posts'/>
 
+      </MainContainer>
       <Footer/>
     </div>
   )
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,7 @@
 function MainContainer({children}){
 
   return(
-    <div className="m-auto max-w-4xl">
+    <div className="m-auto max-w-4xl border">
       {children}
     </div>
   )

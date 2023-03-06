7150367b1e32f8c819c47f1964ba589d16655d89
Gabo <gaabf@hotmail.com>
2023-03-06 15:06:25 -0500

    UPDATE: updated styling in WorkPage component

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -8,21 +8,21 @@ function WorkPage(){
     <div>
       <Header/>
       <MainContainer>
-        <div>
-          <div className="text-3xl font-bold">
+        <div className="py-14">
+          <div className="text-4xl font-bold pb-6">
             Work
           </div>
-          <div>
+          <div className="">
           Est quas et voluptatem vero distinctio. Molestiae eum aliqua..
           </div>
         </div>
 
-        <div>
+        <div className="py-5">
           <div className="text-2xl font-bold text-left">
              Selected Works
           </div>
-          <div className="flex flex-col">
-            <img src='src/assets/images/Rectangle 2banana.png'/>
+          <div className="flex flex-col py-5">
+            <img className='py-3' src='src/assets/images/Rectangle 2banana.png'/>
             <div className="font-bold text-left">Banana Library</div>
             <div className="text-left">
             Possimus sunt autem doloremque enim quo sit architecto reiciendis. Ipsum quam aut aut quo. Est quas et voluptatem vero distinctio. Molestiae eum aliqua...
@@ -50,33 +50,34 @@ function WorkPage(){
               </div>
             </div>
           </div>
-          <div>
-            <div className="font-bold text-2xl text-left">Side Project</div>
+        </div>
 
-            <div className="flex items-center justify-center py-5">
-              <img src='src/assets/images/Rectangle 2small.png'/>
-              <div className="flex flex-col px-5">
-                <div className=" font-bold text-left">Buckinghamshire Croatian Quantify</div>
-                <div className="text-left">Expedita et eveniet non accusantium perspiciatis. Quam deleniti pariatur optio. Quo dicta voluptate...</div>
-                <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
-              </div>
-            </div>
-            <div>
-              <div className="font-bold text-left">Club transform</div>
-              <div className="text-left">
-              Voluptatum vel repudiandae occaecati. Mollitia quae perferendis dolore accusantium vel id. Voluptas perferendis id quasi amet in a qu...
-              </div>
+        <div className="py-5">
+          <div className="font-bold text-2xl text-left">Side Projects</div>
+
+          <div className="flex items-center justify-center py-5">
+            <img src='src/assets/images/Rectangle 2small.png'/>
+            <div className="flex flex-col px-5">
+              <div className=" font-bold text-left">Buckinghamshire Croatian Quantify</div>
+              <div className="text-left">Expedita et eveniet non accusantium perspiciatis. Quam deleniti pariatur optio. Quo dicta voluptate...</div>
               <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
             </div>
           </div>
+          <div>
+            <div className="font-bold text-left">Club transform</div>
+            <div className="text-left">
+            Voluptatum vel repudiandae occaecati. Mollitia quae perferendis dolore accusantium vel id. Voluptas perferendis id quasi amet in a qu...
+            </div>
+            <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>
+          </div>
         </div>
-        <div>
-          <div className="font-bold text-2xl text-left">Experiments & Works in progress</div>
+        <div className="py-5">
+          <div className="font-bold text-2xl text-left py-3">Experiments & Works in progress</div>
           <div>
             <div className="font-bold text-left">
               Rubber
             </div>
-            <div className="text-left">
+            <div className="text-left py-2">
               Aut amet nihil mollitia dolorem commodi quidem unde. Voluptas quaerat numquam quis sunt quas ab asperiores. Iste totam enim voluptas quo...
             </div>
             <div className="text-left text-sm text-gray-600">Sun Jan 29, 2023</div>

b99ce91b1553991fd8a691ee43b3b8cda4013aab
Gabo <gaabf@hotmail.com>
2023-04-18 12:00:56 -0500

    UPDATE: footer responsiveness improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,14 @@ import FooterIcons from "./FooterIcons"
 function Footer(){
 
   return(
-    <div className='flex space-x-60 justify-center items-center h-14'>
-      <div className='text-gray-500 text-sm'>
-        Gabriel Flores | Feel free to drop a 'hello' @ gabriel.flores.2296@gmail.com
+    <div className='px-5 flex md:flex-row flex-col justify-between md:justify-center items-start md:items-center h-14'>
+      <div className="flex flex-col md:flex-row ">
+        <div className='text-gray-500 text-sm text-left min-w-[100px]'>
+          Gabriel Flores |
+        </div>
+        <div className="text-gray-500 text-left text-sm md:px-2">
+          Feel free to drop a 'hello' @ gabriel.flores.2296@gmail.com
+        </div>
       </div>
       <FooterIcons/>
     </div>
--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -81,6 +81,7 @@ function IntroPage(){
       </div>
 
       </MainContainer>
+      <Footer/>
     </div>  
   )
 
